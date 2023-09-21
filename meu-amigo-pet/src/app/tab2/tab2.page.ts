b3ccc052b431b320c2378b0afef50a5f8d8efd6d
Anderson Scherer da Costa <anderson.costa@e-unicred.com.br>
2023-09-20 21:40:10 -0300

    feat: adicionando consumo de dados usuario

--- meu-amigo-pet/src/app/tab2/tab2.page.ts
+++ meu-amigo-pet/src/app/tab2/tab2.page.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Usuario } from '../../model/usuario';
+import { UsersService } from '../api/users.service';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
 
-  constructor() {}
+  usuarios: Usuario[] = [];
+  tipoPessoa = "pessoa_juridica";
+
+  constructor(private serv: UsersService) {}
+
+  ngOnInit(): void {
+    this.buscarPfPj(this.tipoPessoa);
+  }
+
+  atualizarLista = (ev: any) => {
+    console.log(ev)
+    this.serv.getPets().subscribe(res => {
+      this.usuarios = res.filter(u => u.tipo === ev.detail.value);
+    });
+  }
+
+
+  buscarPfPj = (tp: string) => {
+    this.serv.getPets().subscribe(res => {
+      this.usuarios = res.filter(u => u.tipo === tp);
+    });
+  };
 
 }
